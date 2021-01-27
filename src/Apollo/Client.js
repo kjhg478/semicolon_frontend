e7a64ddb2c3a5c23baf0639ad008c78730455434
JHK <31474272+kjhg478@users.noreply.github.com>
2021-01-27 15:13:08 +0900

    Url

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,12 +2,16 @@ import ApolloClient from 'apollo-boost';
 import { defaults, resolvers } from './LocalState';
 
 export default new ApolloClient({
-    uri: "http://localhost:4000",
+    uri: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://semicolonteam-backand.herokuapp.com",
     clientState: {
         defaults,
         resolvers
     },
-    headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
+    request: async operation => {
+        const token = await localStorage.getItem('token');
+        console.log(token);
+        return operation.setContext({
+            headers: { Authorization: `Bearer ${token}` }
+        });
     }
 });
