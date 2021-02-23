060a7bd889f574f61418ac990d0635bff9d776c7
JHK <31474272+kjhg478@users.noreply.github.com>
2021-02-23 10:47:14 +0900

    Server Complete

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri:"https://semicolonteam-backend.herokuapp.com",
+  uri:"https://semicolonteam-backand.herokuapp.com/",
   //  process.env.NODE_ENV === "development"
   //   ? "http://localhost:4000" : ,
   // uri: "http://localhost:4000",
